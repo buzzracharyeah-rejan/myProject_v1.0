16efc87d4b1a21f53512d9d9341a3a2749309f53
rejan buzzracharyeah <rejandev@gmail.com>
2021-12-20 16:18:04 +0545

    fetch coordinates for a given location

---
+++ utils/getCoordinates.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const axios = require('axios');
+
+exports.getCoordinates = async (location) => {
+  const uri = process.env.STACK_URI;
+  const uri_fetch = uri.concat(location);
+  try {
+    const response = await axios.get(uri_fetch);
+    if (response.status >= 200 && response.status < 300) return response.data;
+  } catch (error) {
+    return error;
+  }
+};
