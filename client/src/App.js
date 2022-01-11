8bb6f05bf3af4797cfe83d19c43bb4bfcd965d93
rejan buzzracharyeah <rejandev@gmail.com>
2022-01-12 04:43:24 +0545

    added some pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import { CssBaseline } from '@mui/material';
-// import Signup from './pages/signup/signup';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
 import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
+import Error from './pages/404';
 
 function App() {
   return (
     <>
-      <Login />
+      <Routes>
+        <Route path='/' exact element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </>
   );
 }
